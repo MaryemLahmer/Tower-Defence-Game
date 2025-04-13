615ceb2cf6d65863eb800653c3862ff351dc9cb8
sof14n <147696509+sof14n@users.noreply.github.com>
2025-04-13 17:26:25 +0200

    Wave manager done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,44 @@ public class GameManager : MonoBehaviour
     private static Queue<int> enemyIDsToSummon;
     public bool loopShouldEnd;
     private EnemyWaveManager waveManager;
+    private WaveManager phaseManager;
 
     void Start()
     {
         waveManager = GetComponent<EnemyWaveManager>();
+        phaseManager = FindObjectOfType<WaveManager>();
         enemyIDsToSummon = new Queue<int>();
         EntitySummoner.Init();
         StartCoroutine(GameLoop());
-        InvokeRepeating("SummonTest", 0, 1);
+        
+        // Remove the test summoning since we'll use the wave manager now
+        // InvokeRepeating("SummonTest", 0, 1);
+        
+        // Listen for wave phase start
+        if (phaseManager != null)
+        {
+            phaseManager.onWavePhaseStart.AddListener(OnWavePhaseStarted);
+        }
+    }
+    
+    void OnWavePhaseStarted()
+    {
+        // Wave phase has started, enemies will be spawned by the WaveManager
+        // Any additional setup for the wave phase can go here
     }
 
+    // This can be removed since we're using the WaveManager to spawn enemies
+    /*
     void SummonTest()
     {
         EnqueueEnemyIDToSummon(1);
     }
+    */
 
     IEnumerator GameLoop()
     {
         while (!loopShouldEnd)
         {
-            
-
             if (enemyIDsToSummon.Count > 0)
             {
                 // spawn Enemies 
@@ -52,5 +69,12 @@ public class GameManager : MonoBehaviour
     public static void EnqueueEnemyIDToSummon(int ID)
     {
         enemyIDsToSummon.Enqueue(ID);
+    }
+    
+    // Add a method to notify when all enemies are cleared
+    public void NotifyAllEnemiesCleared()
+    {
+        // This would be called when all enemies from a wave are dead or reached the end
+        // It will help the WaveManager know if we need to end a wave early
     }
 }
